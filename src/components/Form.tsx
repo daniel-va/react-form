da456107ebd7f450e0921ba5dd087bb7f6a8d7b6
Daniel von Atzigen <daniel@vatz.ch>
2022-04-08 11:28:19 +0200

    Submit form on submit event

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import { ReactNode } from 'react'
+import React, { ReactNode, useCallback } from 'react'
 import { FormState, FormStateFields } from '../data/states'
 import { extractFormState } from '../hooks/useForm'
 import FormContext from './FormContext'
@@ -10,10 +9,15 @@ interface Props<T> {
 }
 
 const Form = <T,>({ state, children }: Props<T>): JSX.Element => {
+  const form = extractFormState(state) as FormState<unknown>
+  const handleSubmit = useCallback((e: React.FormEvent) => {
+    e.preventDefault()
+    form.submit()
+  }, [form])
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <FormContext.Provider
-        value={{ state: extractFormState(state) as FormState<unknown> }}
+        value={{ state: form }}
       >
         {children}
       </FormContext.Provider>
